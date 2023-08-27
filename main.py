e503cdb720511f042e155b39b116f2004f1c3a56
ramdhanyA <ariqathallah38@gmail.com>
2023-08-27 20:48:28 +0900

    MeCab implementation failed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import MeCab
 
+input = open("./input.txt", "r").read()
+
 wakati = MeCab.Tagger("-Owakati")
-print(wakati.parse("各種スクリプト言語 (perl, ruby, python, Java) から, MeCab が提供する形態素解析の機能を利用可能です. 各バインディングは SWIG というプログラ ムを用いて, 自動生成されています. SWIG がサポートする他の言語も 生成可能だと思われますが, 現在は, 作者の管理できる範囲内ということで, 上記の4つの言語のみを提供しております.").split())
+result = wakati.parse(input)
+
+
+print(result)
