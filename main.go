9019e8a6f0c7087a3a1ba68444e2f97e6adf6d43
ramdhanyA <ariqathallah38@gmail.com>
2023-08-29 12:47:01 +0900

    Added basic XML structs for parsing

--- main.go
+++ main.go
@@ -34,5 +34,5 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(dict.Entries[0])
+	fmt.Println(dict.Entries[206000])
 }
--- utils/xml.go
+++ utils/xml.go
@@ -10,6 +10,24 @@ type JMdict struct {
 }
 
 type Entry struct {
-	XMLName  xml.Name `xml:"entry"`
-	Sequence int      `xml:"ent_seq"`
+	XMLName        xml.Name       `xml:"entry"`
+	Sequence       int            `xml:"ent_seq"`
+	ReadingElement ReadingElement `xml:"r_ele"`
+	KElement       KElement       `xml:"k_ele"`
+	Sense          Sense          `xml:"sense"`
+}
+
+type ReadingElement struct {
+	XMLName xml.Name `xml:"r_ele"`
+	Reb     string   `xml:"reb"`
+}
+
+type Sense struct {
+	XMLName xml.Name `xml:"sense"`
+	Gloss   string   `xml:"gloss"`
+}
+
+type KElement struct {
+	XMLName xml.Name `xml:"k_ele"`
+	Keb     string   `xml:"keb"`
 }
