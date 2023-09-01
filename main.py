50b3738cfebd13aaf3506a76efcf9e39742b0a0b
ramdhanyA <ariqathallah38@gmail.com>
2023-09-01 21:01:36 +0900

    parsed xml

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hi, I'm Ankigen in Python!")
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('JMdict_e.xml')
+root = tree.getroot()
+
+print(root[1000].find("sense").find("gloss").text)
