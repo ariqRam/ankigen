c682acbe469f68eab11bfa94ff2a68bca61ba096
ramdhanyA <ariqathallah38@gmail.com>
2023-09-03 08:38:56 +0900

    pytesseract implementation added but reading is still unclear

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import xml.etree.ElementTree as ET
+import utils.dict as dict
+import utils.ocr as ocr
 
-tree = ET.parse('JMdict_e.xml')
-root = tree.getroot()
-
-# print(root[1000].find("sense").find("gloss").text)
-
-def lookup(word: str):
-    for entry in root.findall("entry"):
-        try:
-            if entry.find('k_ele').find('keb').text == word:
-                return entry
-        except AttributeError:
-            continue
-
-for idx, expl in zip(range(1,5), lookup("全部").find("sense").findall("gloss")):
+for idx, expl in zip(range(1,5), dict.lookup("全部").find("sense").findall("gloss")):
     print(f"{idx} - {expl.text}")
+
+print(ocr.read("image-jp.png"))
