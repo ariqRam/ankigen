6ad7dfeb04e5140d684c977b7481357521112a06
ramdhanyA <ariqathallah38@gmail.com>
2023-09-03 15:12:41 +0900

    tessdata path configuration

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import utils.ocr as ocr
 for idx, expl in zip(range(1,5), dict.lookup("全部").find("sense").findall("gloss")):
     print(f"{idx} - {expl.text}")
 
-print(ocr.read("image-jp.png"))
+print(ocr.read("hard-stock.png"))
