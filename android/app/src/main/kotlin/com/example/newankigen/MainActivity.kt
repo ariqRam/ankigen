4a6ac3ff01dd670ab15cbf05201b3e68ad9fb0ca
ramdhanyA <ariqathallah38@gmail.com>
2023-11-06 21:08:11 +0900

    camera screen added

---
+++ android/app/src/main/kotlin/com/example/newankigen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newankigen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/camera_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({
+    super.key,
+    required this.camera,
+  });
+  final CameraDescription camera;
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the Future is complete, display the preview.
+          return CameraPreview(_controller);
+        } else {
+          // Otherwise, display a loading indicator.
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
